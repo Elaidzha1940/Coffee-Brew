5bb762a3e913b501a7829d6c4202f8289f0dd24a
Elaidzha1940 <eli080293@gmail.com>
2023-07-15 21:03:27 +0300

    CoffeeBrew / I will make 3 UI

---
+++ Coffee Brew/Coffee Brew/UI/Views/Description.swift
@@ -0,0 +1,22 @@
+//  /*
+//
+//  Project: Coffee Brew
+//  File: Description.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 15.07.2023
+//
+//  */
+
+import SwiftUI
+
+struct Description: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Description_Previews: PreviewProvider {
+    static var previews: some View {
+        Description()
+    }
+}
---
+++ Coffee Brew/Coffee Brew/UI/Views/Home.swift
@@ -0,0 +1,28 @@
+//  /*
+//
+//  Project: Coffee Brew
+//  File: Home.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 15.07.2023
+//
+//  */
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        
+        VStack {
+            
+            
+            Image("system-bigcoffee")
+        }
+        .background(Color(toElement: .main))
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
