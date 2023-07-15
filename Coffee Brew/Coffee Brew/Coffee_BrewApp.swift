3f9c92e5b32cf8ffb59276e13741e8a19dbfbfb9
Elaidzha1940 <eli080293@gmail.com>
2023-07-15 20:20:13 +0300

    Coffee Brew

---
+++ Coffee Brew/Coffee Brew/Coffee_BrewApp.swift
@@ -0,0 +1,21 @@
+//  /*
+//
+//  Project: Coffee Brew
+//  File: Coffee_BrewApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 15.07.2023
+//
+//  S
+//
+//  */
+
+import SwiftUI
+
+@main
+struct Coffee_BrewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
