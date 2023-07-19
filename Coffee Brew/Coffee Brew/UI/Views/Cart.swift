73f9751aaeebb15bee30e47d1f9fa782d14d2118
Elaidzha1940 <eli080293@gmail.com>
2023-07-19 20:02:43 +0300

    Cart

--- Coffee Brew/Coffee Brew/UI/Views/Cart.swift
+++ Coffee Brew/Coffee Brew/UI/Views/Cart.swift
@@ -23,6 +23,82 @@ struct Cart: View {
                     
                     VStack {
                         
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 410, height: 120)
+                                .foregroundColor(Color(toElement: .cartrect))
+                                .cornerRadius(12)
+                            
+                            HStack {
+                                Image("system-macha")
+                                    .resizable()
+                                    .frame(width: 100, height: 100)
+                                
+                                VStack(alignment: .leading, spacing: 10) {
+                                    Text("Capuccino")
+                                        .font(.custom(Fonts.BonaNova.regular, size: 17))
+                                        .foregroundColor(.white)
+                                    Text("Dalgona Macha")
+                                        .font(.custom(Fonts.BonaNova.regular, size: 17))
+                                        .foregroundColor(.white)
+                                    Text("R 248")
+                                        .font(.custom(Fonts.BonaNova.bold, size: 17))
+                                        .foregroundColor(.white)
+                                }
+                                //1.
+                            }
+                        }
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 410, height: 120)
+                                .foregroundColor(Color(toElement: .cartrect))
+                                .cornerRadius(12)
+                            
+                            HStack {
+                                Image("system-blueberry")
+                                    .resizable()
+                                    .frame(width: 100, height: 100)
+                                
+                                VStack(alignment: .leading, spacing: 10) {
+                                    Text("Capuccino")
+                                        .font(.custom(Fonts.BonaNova.regular, size: 17))
+                                        .foregroundColor(.white)
+                                    Text("Bursting Bluberry")
+                                        .font(.custom(Fonts.BonaNova.regular, size: 17))
+                                        .foregroundColor(.white)
+                                    Text("R 99")
+                                        .font(.custom(Fonts.BonaNova.bold, size: 17))
+                                        .foregroundColor(.white)
+                                }
+                                //2.
+                            }
+                        }
+                        
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 410, height: 120)
+                                .foregroundColor(Color(toElement: .cartrect))
+                                .cornerRadius(12)
+                            
+                            HStack {
+                                Image("system-cocoa")
+                                    .resizable()
+                                    .frame(width: 100, height: 100)
+                                
+                                VStack(alignment: .leading, spacing: 10) {
+                                    Text("Capuccino")
+                                        .font(.custom(Fonts.BonaNova.regular, size: 17))
+                                        .foregroundColor(.white)
+                                    Text("Cinnamon & Cocoa")
+                                        .font(.custom(Fonts.BonaNova.regular, size: 17))
+                                        .foregroundColor(.white)
+                                    Text("R 299")
+                                        .font(.custom(Fonts.BonaNova.bold, size: 17))
+                                        .foregroundColor(.white)
+                                }
+                                //3.
+                            }
+                        }
                     }
                 }
             }
