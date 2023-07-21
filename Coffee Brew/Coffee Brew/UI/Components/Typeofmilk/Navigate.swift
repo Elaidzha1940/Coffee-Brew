4a5a14babcd57f897a3704880ea01ad6e159a532
Elaidzha1940 <eli080293@gmail.com>
2023-07-21 19:49:26 +0300

    added / Navigation + Tabbar Files

---
+++ Coffee Brew/Coffee Brew/UI/Components/Typeofmilk/Navigate.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: Coffee Brew
+//  File: Navigate.swift
+//  Created by: Elaidzha Shchukin on 21.07.2023.
+//  Date: 21.07.2023
+//
+//  */
+
+import SwiftUI
+
+struct Navigate: View {
+    var body: some View {
+        
+        Text("Navigaete!")
+    }
+}
+
+struct Navigate_Previews: PreviewProvider {
+    static var previews: some View {
+        Navigate()
+    }
+}
---
+++ Coffee Brew/Coffee Brew/UI/Components/Typeofmilk/Tabbar.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: Coffee Brew
+//  File: Tabbar.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 21.07.2023
+//
+//  */
+
+import SwiftUI
+
+struct Tabbar: View {
+    var body: some View {
+        
+        Text("Tabbar")
+    }
+}
+
+struct Tabbar_Previews: PreviewProvider {
+    static var previews: some View {
+        Tabbar()
+    }
+}
