213e4bde06673e1ac3e3811330f8245053680b38
Elaidzha1940 <eli080293@gmail.com>
2023-07-25 15:31:59 +0300

    added Spesificstar is done

--- Coffee Brew/Coffee Brew/UI/Components/Spesificcorners/Spesificstar.swift
+++ Coffee Brew/Coffee Brew/UI/Components/Spesificcorners/Spesificstar.swift
@@ -14,8 +14,8 @@ struct Spesificstar: View {
         
         ZStack {
             Rectangle()
-                .frame(width: 50, height: 50, alignment: .center)
-                .foregroundColor(Color(toElement: .couponcode))
+                .frame(width: 70, height: 35, alignment: .center)
+                .foregroundColor(Color(toElement: .couponcode).opacity(0.2))
             
             HStack {
                 Image("system-star")
@@ -25,11 +25,30 @@ struct Spesificstar: View {
                     .foregroundColor(.white)
             }
         }
+        .cornerRadius(50, corners: .topLeft)
+        .cornerRadius(50, corners: .bottomRight)
     }
 }
 
 struct Spesificstar_Previews: PreviewProvider {
     static var previews: some View {
         Spesificstar()
+    }
+}
+
+struct RoundedCorner1: Shape {
+    
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func cornerRadius1(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
     }
 }
